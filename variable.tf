59f404e53003098eaf8834205af570204481b196
Nour <megahed1983nour@gmail.com>
2018-03-06 02:56:48 +0000

    Committing modules

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable modname {}
+variable SubnetId {
+default = "subnet-d17a5cfe"
+ description = "The subnet id of a default vpc in the working region"
+}
+variable SecGroupId {
+description = "this variable doesn't have a default value but it is passed from the main code"
+}
